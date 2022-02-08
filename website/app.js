0da4bc31d3929d9f91f0cf0085be00f3eb3d1716
George <gzald001@gmail.com>
2022-02-07 20:04:48 -0500

    feat: Post application weather data

--- website/app.js
+++ website/app.js
@@ -1,30 +1,37 @@
-const { get } = require("http");
-
 /* Global Variables */
-const baseUrl = 'api.openweathermap.org'
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip='
 
 // Personal API Key for OpenWeatherMap API
-const apiKey = '92f2bfc4f07c3ec685481edf7d16a935&units=imperial';
+const apiKey = '&appid=92f2bfc4f07c3ec685481edf7d16a935&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-const getTemperature = async url => {
-    await fetch(url, {
-        method: 'GET',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'applications/json',
-        },
-        body: JSON.stringify(data)
+const getWeatherData = async () => {
+    const zipcode = document.getElementById('zip').value;
+    const url = baseUrl + zipcode + apiKey;
+
+    await fetch(url)
+    .then(async response => await response.json())
+    .then(async data => {
+        const feelingsToday = document.getElementById('feelings').value;
+        const objData = { temperature: data.main.temp, date: newDate, feelings: feelingsToday };
+
+        await fetch('/addTemp', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(objData)
+        })
+        .then(async response => await response.json())
+        .catch(error => console.log('Post Error: ', error))
     })
-    .then()
+    .catch(error => console.log('Get Error: ', error))
     
 }
 
-let zipcode = document.getElementById('zip').value;
-let fullUrl = baseUrl + zipcode + apiKey;
-
 const button = document.getElementById('generate');
-button.addEventListener('click', getTemperature(fullUrl)); 
+button.addEventListener('click', getWeatherData); 
