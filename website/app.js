39d429eb4020d6b0683b5fde0a1258e5a2c39d96
George <gzald001@gmail.com>
2022-02-08 09:50:29 -0500

    feat: Dynamically update UI

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 
 // Personal API Key for OpenWeatherMap API
 const apiKey = '&appid=92f2bfc4f07c3ec685481edf7d16a935&units=imperial';
@@ -18,7 +18,7 @@ const getWeatherData = async () => {
         const feelingsToday = document.getElementById('feelings').value;
         const objData = { temperature: data.main.temp, date: newDate, feelings: feelingsToday };
 
-        await fetch('/addTemp', {
+        await fetch('/addToProjectData', {
             method: 'POST',
             credentials: 'same-origin',
             headers: {
@@ -27,11 +27,19 @@ const getWeatherData = async () => {
             body: JSON.stringify(objData)
         })
         .then(async response => await response.json())
+        .then(data => {
+            const lastElement = data.length - 1;
+            document.getElementById('date').innerHTML = `Date: ${data[lastElement].date}`;
+            document.getElementById('temp').innerHTML = `Temperature: ${Math.round(data[lastElement].temperature)}`;
+            document.getElementById('content').innerHTML = `Feeling ${data[lastElement].feelings} today`;
+            document.getElementById('feelings').value = '';
+            document.getElementById('zip').value = '';
+        })
         .catch(error => console.log('Post Error: ', error))
     })
     .catch(error => console.log('Get Error: ', error))
-    
-}
+
+};
 
 const button = document.getElementById('generate');
 button.addEventListener('click', getWeatherData); 
